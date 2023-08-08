8ce191388670a912e560e3168a9baed82745966b
ruzsakrisztian <ruzsakrisztian@hotmail.com>
2023-08-08 19:15:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const shortNames = require("./04-names");
+const sayHi = require("./05-utils");
+const data = require("./06-alternative-flavor");
+console.log(data);
+sayHi("Susan");
+sayHi(shortNames.john);
+sayHi(shortNames.peter);
